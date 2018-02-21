9543a4c143f37559ef8e7696c23b6de651185627
Tim Davis <timdavish@gmail.com>
2018-02-20 17:09:49 -0800

    Add hero animation

--- js/index.js
+++ js/index.js
@@ -1,24 +1,67 @@
 "use strict"
 
-const main = $('main')
-const loader = $('.loader')
-const HIDDEN = 'hidden'
+const viewportWidth = window.innerWidth
+const viewportHeight = window.innerHeight
+const heroBackgroundParts = []
+const colorArray = [
+  '#DBE2EF',
+  '#D53939',
+  '#90D26D',
+  '#FFEA85',
+  '#53354A',
+]
 
-if (browserSupportsCSSProperty('animation')) {
+let lastColorIndex = 0
+let uniqueId = Number.MAX_SAFE_INTEGER
+
+if (browserSupportsCSSProperty('animations')) {
   // Show the loader for a random amount of time
-  const randomTime = (1.5 + Math.random()) * 1000
-
-  loader.classList.remove(HIDDEN)
-  setTimeout(() => {
-    loader.classList.add(HIDDEN)
-    main.classList.remove(HIDDEN)
-  }, randomTime)
+  const randomTime = (1 + Math.random()) * 1000
+  setTimeout(loadPage, randomTime)
 } else {
-  main.classList.remove(HIDDEN)
+  loadPage()
 }
 
-function $ (selector) {
-  return document.querySelector(selector)
+function loadPage () {
+  $('.loader').addClass('hidden')
+  $('main').removeClass('hidden')
+  $('main').addClass('loaded')
+
+  setTimeout(animateHero, 0)
+}
+
+function animateHero () {
+  if (heroBackgroundParts.length >= 6) {
+    $(`#${heroBackgroundParts.shift()}`).remove()
+  }
+
+  const id = (uniqueId--).toString(16)
+  const moveX = getRandom(0, viewportWidth)
+  const moveY = getRandom(0, viewportHeight)
+
+  heroBackgroundParts.push(id)
+  $('main').append(`<div class="hero_background_part" id="${id}"></div>`)
+
+  let colorIndex = getRandom(0, colorArray.length - 1)
+  while (lastColorIndex === colorIndex) colorIndex = getRandom(0, colorArray.length - 1)
+  lastColorIndex = colorIndex
+
+  setTimeout(() => {
+    $(`#${id}`).css({
+      left: `${moveX / 2}px`,
+      top: `${moveY / 2}px`,
+      width: viewportHeight,
+      height: viewportHeight,
+      transform: `scale3d(3, 3, 3)`,
+      'background-color': colorArray[colorIndex]
+    })
+  }, 100)
+
+  setTimeout(animateHero, 10000)
+}
+
+function getRandom (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function browserSupportsCSSProperty (propertyName) {
